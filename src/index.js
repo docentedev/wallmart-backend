2c9dbfd438283ca45c1cff6d246e85322b598f78
Claudio Rojas <cerojasr@falabella.cl>
2021-01-17 04:53:51 -0300

    Code style to index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const cors = require('cors')
-
+const dotenv = require('dotenv')
 const healthRotuer = require('./routers/health')
 const productsRotuer = require('./routers/products')
 const discountsRotuer = require('./routers/discounts')
 
+dotenv.config()
+
 const app = express()
 app.use(cors({
-    "origin": "*",
-    "methods": "GET,HEAD",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }))
+  origin: '*',
+  methods: 'GET,HEAD',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}))
 
 const port = process.env.PORT || 3001
 
